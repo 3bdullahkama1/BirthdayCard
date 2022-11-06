1999cbf8955e2016cf156ce2f87e7369112af9a9
3bdullahkama1 <abdallah10kamal@gmail.com>
2022-11-06 15:28:25 +0200

    Add Background image

--- lib/home.dart
+++ lib/home.dart
@@ -5,6 +5,15 @@ import 'package:flutter/src/widgets/framework.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFFe967e1),
+      body: Center(
+        child: Image(
+          image: AssetImage(
+            'images/birthday_card_background.jpg',
+          ),
+        ),
+      ),
+    );
   }
 }
